a29553f2bdc32597cf404da5e98513e1a875110f
letsmespeakfrommyheart <maria@MacBook-Air-Vladimir.local>
2022-07-30 18:44:57 +0300

    add app

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Default Browser App'),
+            backgroundColor: Colors.blue),
+        body: SafeArea(
+          child: Center(
+              child: TextButton(
+            child: const Text(
+              "Open flutter.dev",
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () async {
+              var url = Uri.parse('https://flutter.dev');
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url, mode: LaunchMode.externalApplication);
+              } else {
+                throw 'Could not launch $url';
+              }
+            },
+          )),
+        ));
+  }
+}
